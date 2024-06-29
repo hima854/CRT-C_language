c91cd26275389ac1e13a407ccbd757de7ab158e0
hima854 <botsahimabindu2002@gmail.com>
2024-06-29 11:46:52 +0530

    Add files via upload

---
+++ codeforces_466A.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+main()
+{
+	int n,m,a,b,res,x;
+	scanf("%d %d %d %d",&n,&m,&a,&b);
+	if(a>(b/m))
+	{
+		x=n%m;
+		res=b*(n/m);
+		if(x!=0)
+		{
+		  if(b<(x*a))
+		  {
+		   	 res=res+b;
+		  	 printf("%d",res);
+		  }
+		  else
+		  {
+		  	 res=res+(x*a);
+		  	 printf("%d",res);
+		  }
+		}
+		else
+		{
+	      printf("%d",res);
+		}
+	}
+	else
+	{
+		res=a*n;
+		printf("%d",res);
+	}
+}
